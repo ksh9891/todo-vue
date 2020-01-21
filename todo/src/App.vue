f2351c85600863b72f004d6fd23be4430a3a2599
SangHyun Kim <ksh9891@byulsoft.com>
2020-01-21 14:55:44 +0900

    front-end vue-cli created

---
+++ todo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <HelloWorld msg="하이"></HelloWorld>
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+    name: 'app',
+    components: {
+        HelloWorld
+    }
+}
+</script>
